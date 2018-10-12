1d2d86d86a8c46b884278befdd5d1654d5aa793d
TaniaOzuna <al02717978@tecmilenio.mx>
2018-10-11 22:52:59 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ReadCards
 # ReadCards
+# ReadCards
---
+++ app/src/main/java/com/example/readcards/readcards/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.readcards.readcards;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int MY_SCAN_REQUEST_CODE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button scan_buton = (Button)findViewById(R.id.scan_button);
+        scan_buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onScanPress(view);
+            }
+        });
+    }
+
+    public void onScanPress(View v){
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, true); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_SCAN_REQUEST_CODE) {
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+                TextView creditCardNumber = (TextView)findViewById(R.id.creditcardnumber);
+                creditCardNumber.setText(scanResult.getRedactedCardNumber());
+
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                    TextView expiryMonth =(TextView)findViewById(R.id.expiryMonth);
+                    expiryMonth.setText(scanResult.expiryMonth + "/" + scanResult.expiryYear);
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                    TextView cvv =(TextView)findViewById(R.id.cvv);
+                    cvv.setText(scanResult.cvv);
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                    TextView postalCode =(TextView)findViewById(R.id.postalCode);
+                    postalCode.setText(scanResult.postalCode);
+                }
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultDisplayStr);
+        }
+        // else handle other activity results
+    }
+    }
+
